7089d5ad8212fa2a060ca90f2e4640f072aa0ea6
sGxOxDs <koyoyo168@gmail.com>
2024-07-26 11:14:55 +0800

    更新 讀取TG資料

--- WebTest/Tonshi/ServiceWorker.js
+++ WebTest/Tonshi/ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "Rumuinno-Tonshi-0.1";
+const contentToCache = [
+    "Build/TG-Web.loader.js",
+    "Build/TG-Web.framework.js.unityweb",
+    "Build/TG-Web.data.unityweb",
+    "Build/TG-Web.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
