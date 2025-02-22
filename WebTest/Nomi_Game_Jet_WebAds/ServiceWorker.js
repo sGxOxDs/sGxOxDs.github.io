208292bfe53447e1bb6e912ec74ab17fd8754d64
sGxOxDs <62545840+sGxOxDs@users.noreply.github.com>
2025-02-22 22:20:37 +0800

    Web Ads Test

---
+++ WebTest/Nomi_Game_Jet_WebAds/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "sGxOxDs-Nomi GameJet-0.4";
+const contentToCache = [
+    "Build/PWA_0_4.loader.js",
+    "Build/PWA_0_4.framework.js.unityweb",
+    "Build/PWA_0_4.data.unityweb",
+    "Build/PWA_0_4.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
